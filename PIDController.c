7a1c9cc51fbb4cbaa7b6a6cc28e7798a085f4891
jayden-chan <32287958+jayden-chan@users.noreply.github.com>
2018-02-06 21:42:48 -0800

    Initial code dump

---
+++ PIDController.c
@@ -0,0 +1,81 @@
+/*
+	Author: Jayden Chan
+	Date Created: Jan 12 2018
+	Last Modified: Jan 22 2018
+	Details: PID Controller
+*/
+
+#ifndef PIDController.c
+#define PIDController.c
+
+#include "Utils.c"
+#include "Constants.h"
+
+typedef struct {
+	float P, I, D;
+	float error;
+	float errorSum, lastError;
+	int lastTime;
+	bool zeroOnCross;
+	float integralLimit, epsilon;
+	float dTime;
+} PID;
+
+// "Constructor" for the PID controller
+void PIDInit(PID &pid, float kP, float kI, float kD, float integralLimit, float epsilon, bool zeroOnCross)
+{
+	pid.P = kP;
+	pid.I = kI;
+	pid.D = kD;
+
+	pid.integralLimit = integralLimit;
+	pid.epsilon = epsilon;
+	pid.zeroOnCross = zeroOnCross;
+}
+
+// Resets the error values for the provided PID controller
+void PIDReset(PID &pid)
+{
+	pid.errorSum = 0;
+	pid.lastError = 0;
+}
+
+float PIDCalculate(PID &pid, float error)
+{
+	// Update time
+	pid.dTime = nSysTime - pid.lastTime;
+	pid.lastTime = nSysTime;
+
+	// Update error
+	pid.lastError = pid.error;
+	pid.error = error;
+
+	// Calculate the change in error if the elapsed time is not zero
+	float changeInError = pid.dTime != 0 ? (pid.error - pid.lastError) / pid.dTime : 0;
+
+	// Reset the error sum when the error crosses zero if that setting is enabled
+	if(pid.zeroOnCross && (sign(pid.error) != sign(pid.lastError)))
+	{
+		pid.errorSum = 0;
+	}
+
+	// Add the P and D values to the output sum
+	float output = pid.P * pid.error + pid.D * changeInError;
+
+	if(abs(output) < MAX_SPEED)
+	{
+		// Calculate the integral of error if it is above the epsilon threshold
+		pid.errorSum = abs(pid.error) > pid.epsilon ? pid.errorSum + pid.error * pid.dTime : 0;
+	}
+
+	// Clamp the integral to the provided integral limit
+	pid.errorSum = clamp(pid.errorSum, pid.integralLimit);
+
+	// Add the I term to the sum
+	output += pid.I * pid.errorSum;
+
+	// Return the sum
+	return output;
+}
+
+#endif
---
+++ Utils.c
@@ -0,0 +1,28 @@
+/*
+	Author: Jayden Chan
+	Date Created: Jan 12 2018
+	Last Modified: Jan 13 2018
+	Details: Utility class to support PIDController and other files
+*/
+
+#ifndef Utils.c
+#define Utils.c
+
+// Returns the sign of the input, and 0 if the input is 0
+float sign(float input)
+{
+	if(input == 0)
+	{
+		return 0;
+	}
+
+	return abs(input) / input;
+}
+
+// Clamps the input value between +clamp and -clamp
+float clamp(float input, float clamp)
+{
+	return abs(input) > clamp ? clamp * sign(input) : input;
+}
+
+#endif
