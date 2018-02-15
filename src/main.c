f56f805dd4b6c8db976035347832d3cf63386c2f
jayden-chan <jaydencn7@gmail.com>
2018-02-15 00:35:07 -0700

    Trying to fix dumb driving bug

--- src/main.c
+++ src/main.c
@@ -56,11 +56,11 @@ void init() {
 }
 
 void testPeriodic() {
-    driveReset();
+    driveInit();
     arcTurn(30, 90, true, 20, 250);
-    driveReset();
+    driveInit();
     driveStraight(100, 70, 20, 250);
-    driveReset();
+    driveInit();
     arcTurn(30, 90, false, 20, 250);
 
     currentState = STATE_DISABLED;
